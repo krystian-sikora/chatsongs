a295c36c4103037ae3c04270e8fcf6a16a3a40d0
krystian-sikora <s223097@student.uek.krakow.pl>
2024-07-15 20:14:59 +0200

    fixes SpringBootTest

--- src/test/java/pl/ksikora/chatsongs/FilmReviewerBackendApplicationTests.java
+++ src/test/java/pl/ksikora/chatsongs/FilmReviewerBackendApplicationTests.java
@@ -6,6 +6,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FilmReviewerBackendApplicationTests {
 
+    static {
+        System.setProperty("JWT_SECRET_KEY", ":)");
+        System.setProperty("FRONTEND_APP_URL", ":)");
+        System.setProperty("SPOTIFY_CLIENT_ID", ":)");
+        System.setProperty("SPOTIFY_CLIENT_SECRET", ":)");
+    }
+
     @Test
     void contextLoads() {
     }
