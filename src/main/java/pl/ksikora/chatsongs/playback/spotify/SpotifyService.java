3ff13b6da432658370855889233ef04f1ae70bf7
krystian-sikora <s223097@student.uek.krakow.pl>
2024-07-17 22:24:47 +0200

    adds scopes due to issues

--- src/main/java/pl/ksikora/chatsongs/playback/spotify/SpotifyService.java
+++ src/main/java/pl/ksikora/chatsongs/playback/spotify/SpotifyService.java
@@ -67,7 +67,7 @@ public class SpotifyService {
         SpotifyApi spotifyApi = spotifyConfiguration.getSpotifyObject();
 
         String state = generateRandomString(16);
-        String scope = "streaming, user-read-birthdate, user-read-email, user-read-private, user-read-playback-state, user-modify-playback-state, user-read-currently-playing";
+        String scope = "streaming, user-read-email, user-read-private, user-read-playback-state, user-modify-playback-state, user-read-currently-playing";
 
         AuthorizationCodeUriRequest authorizationCodeRequest = spotifyApi.authorizationCodeUri()
                 .state(state)
